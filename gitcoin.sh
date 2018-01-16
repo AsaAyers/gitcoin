00e0ff3ab647ed40b0c72cba6f65b2ca26b81b84
Asa Ayers <Asa@AsaAyers.com>
2018-01-15 23:41:58 -0800

    Add garbage collection when a hash is found

--- gitcoin.sh
+++ gitcoin.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 DIFFICULTY=2
-PREFIX=$(head -c $DIFFICULTY < /dev/zero | tr '\0' '0')
-CURRENT=$(git rev-parse --verify HEAD)
 
+PREFIX=$(head -c $DIFFICULTY < /dev/zero | tr '\0' '0')
+
+CURRENT=$(git rev-parse --verify HEAD)
 if [[ $CURRENT == $PREFIX* ]]; then
   echo 'Please add a new commit to mine'
   exit 1
 fi
 
-CURRENT=$(git rev-parse --verify HEAD)
 while [[ $CURRENT != $PREFIX* ]];do
   echo $CURRENT | tee .gitcoin-nonce
   git add .gitcoin-nonce
@@ -17,4 +17,7 @@ while [[ $CURRENT != $PREFIX* ]];do
   git commit --amend -C HEAD --no-edit > /dev/null
   CURRENT=$(git rev-parse --verify HEAD)
 done
+
 echo "SUCCESS: $CURRENT"
+git count-objects
+git gc --auto
