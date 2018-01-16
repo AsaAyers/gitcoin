00d7fd5c511daefb0251e795b60f7ac44eebc557
Asa Ayers <Asa@AsaAyers.com>
2018-01-15 23:36:36 -0800

    :tada: Initial commit for GitCoin

---
+++ gitcoin.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+DIFFICULTY=2
+PREFIX=$(head -c $DIFFICULTY < /dev/zero | tr '\0' '0')
+CURRENT=$(git rev-parse --verify HEAD)
+
+if [[ $CURRENT == $PREFIX* ]]; then
+  echo 'Please add a new commit to mine'
+  exit 1
+fi
+
+CURRENT=$(git rev-parse --verify HEAD)
+while [[ $CURRENT != $PREFIX* ]];do
+  echo $CURRENT | tee .gitcoin-nonce
+  git add .gitcoin-nonce
+
+  git commit --amend -C HEAD --no-edit > /dev/null
+  CURRENT=$(git rev-parse --verify HEAD)
+done
+echo "SUCCESS: $CURRENT"
